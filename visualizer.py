1754d9db7c9fa7d8d4dadf50c51ba5b250d2dc1b
alex-chen1 <alexchen1@berkeley.edu>
2025-04-21 01:27:01 -0700

    design of the game board done, it currently scales with screen size but not board size properly

--- visualizer.py
+++ visualizer.py
@@ -6,34 +6,164 @@ import numpy as np
 # pixels are black or white  using a 640x480 array.
 def design():
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
+    screen_x = 1000
+    screen_y = 1000
+    screen = pygame.display.set_mode((screen_x, screen_y))
     pygame.display.set_caption("Black and White Display")
     
     running = True
     
     # Create a 640x480 array filled with 0 (black)
-    pixel_array = np.zeros((480, 640), dtype=np.uint8)
+    pixel_array = np.zeros((screen_y, screen_x), dtype=np.uint8)
+
+    # define size of tetris blocks
+    side = 18
+    space = 2
+
+    # define size of game board
+    rows = 25
+    cols = 15
 
     # draw a board full of blocks
-    side = 18
-    space = 2 
-    vstart = (480 - 20 * (side + space)) // 2
-    hstart = (640 - 10 * (side + space)) // 2
+    # define vertical and horizontal start/end points for the game board (not the full display, just the play area)
+    vstart = (screen_y - rows * (side + space)) // 2
+    hstart = (screen_x - cols * (side + space)) // 2 - 100
+    vend = vstart + rows * (side + space)
+    hend = hstart + cols * (side + space)
     for i in range(side):
-        for col in range(10):
-            pixel_array[vstart + i:(vstart + i + (side + space) * 20):(side + space), hstart + col * (side + space):hstart + col * (side + space) + side] = 255
+        for col in range(cols):
+            pixel_array[vstart + i + space // 2:(vstart + i + (side + space) * rows) + space // 2:(side + space), hstart + col * (side + space) + space // 2:hstart + col * (side + space) + side + space // 2] = 255
     
     # draw the next block tile
-    for x in range(80, 240):
-        pixel_array[x, 450:610] = 200
+    for i in range(side):
+        for col in range(4):
+            pixel_array[vstart + 120 + i + space // 2:(vstart + 120 + i + (side + space) * 4):(side + space) + space // 2, hstart + 250 + col * (side + space) + space // 2:hstart + 250 + col * (side + space) + side + space // 2] = 255
     
-    pixel_array[50:70, 450:453] = 255
+    # draw "NEXT BLOCK" text
+    # draw N
+    pixel_array[vstart + 10:vstart + 30, hend + 50:hend + 53] = 255
     for i in range(1,12):
-        pixel_array[]
-    pixel_array[50:70, 462:465] = 255
+        pixel_array[vstart + 11 + i:vstart + 15 + i, hend + 50 +i] = 255
+    pixel_array[vstart + 10:vstart + 30, hend + 62:hend + 65] = 255
+    # draw E
+    pixel_array[vstart + 10:vstart + 30, hend + 70:hend + 73] = 255
+    pixel_array[vstart + 10:vstart + 13, hend + 73:hend + 85] = 255
+    pixel_array[vstart + 18:vstart + 21, hend + 73:hend + 85] = 255
+    pixel_array[vstart + 27:vstart + 30, hend + 73:hend + 85] = 255
+    # draw X
+    for i in range(0,15):
+        pixel_array[vstart + 11 + i:vstart + 15 + i, hend + 90 + i] = 255
+        pixel_array[vstart + 11 + i:vstart + 15 + i, hend + 104 - i] = 255
+    pixel_array[vstart + 10:vstart + 15, hend + 90:hend + 95] = 255
+    pixel_array[vstart + 25:vstart + 30, hend + 90:hend + 95] = 255
+    pixel_array[vstart + 10:vstart + 15, hend + 100:hend + 105] = 255
+    pixel_array[vstart + 25:vstart + 30, hend + 100:hend + 105] = 255
+    # draw T
+    pixel_array[vstart + 10:vstart + 30, hend + 116:hend + 120] = 255
+    pixel_array[vstart + 10:vstart + 13, hend + 110:hend + 125] = 255
+    # draw B
+    pixel_array[vstart + 35:vstart + 55, hend + 40:hend + 43] = 255
+    pixel_array[vstart + 35, hend + 40:hend + 53] = 255
+    pixel_array[vstart + 36, hend + 40:hend + 54] = 255
+    pixel_array[vstart + 37, hend + 40:hend + 55] = 255
+    pixel_array[vstart + 43, hend + 40:hend + 54] = 255
+    pixel_array[vstart + 44, hend + 40:hend + 55] = 255
+    pixel_array[vstart + 45, hend + 40:hend + 54] = 255
+    pixel_array[vstart + 52, hend + 40:hend + 55] = 255
+    pixel_array[vstart + 53, hend + 40:hend + 54] = 255
+    pixel_array[vstart + 54, hend + 40:hend + 53] = 255
+    pixel_array[vstart + 38:vstart + 52, hend + 52:hend + 55] = 255
+    # draw L
+    pixel_array[vstart + 35:vstart + 55, hend + 60:hend + 63] = 255
+    pixel_array[vstart + 52:vstart + 55, hend + 60:hend + 75] = 255
+    # draw O
+    pixel_array[vstart + 35:vstart + 55, hend + 80:hend + 83] = 255
+    pixel_array[vstart + 35:vstart + 55, hend + 92:hend + 95] = 255
+    pixel_array[vstart + 35:vstart + 38, hend + 80:hend + 95] = 255
+    pixel_array[vstart + 52:vstart + 55, hend + 80:hend + 95] = 255
+    # draw C
+    pixel_array[vstart + 35:vstart + 55, hend + 100:hend + 103] = 255
+    pixel_array[vstart + 35:vstart + 38, hend + 100:hend + 115] = 255
+    pixel_array[vstart + 52:vstart + 55, hend + 100:hend + 115] = 255
+    # draw K
+    for i in range(9):
+        pixel_array[vstart + 42 + i:vstart + 47 + i, hend + 122 + i] = 255
+    for i in range(9):
+        pixel_array[vstart + 35 + i:vstart + 40 + i, hend + 130 - i] = 255
+    pixel_array[vstart + 35:vstart + 55, hend + 120:hend + 123] = 255
+
+    pixel_array[vstart + 35:vstart + 39, hend + 131] = 255
+    pixel_array[vstart + 35:vstart + 38, hend + 132] = 255
+    pixel_array[vstart + 35:vstart + 37, hend + 133] = 255
+
+    pixel_array[vstart + 51:vstart + 55, hend + 131] = 255
+    pixel_array[vstart + 52:vstart + 55, hend + 132] = 255
+    pixel_array[vstart + 53:vstart + 55, hend + 133] = 255
 
     # draw the number of lines tile
+    # draw L
+    pixel_array[vstart + 310:vstart + 330, hend + 40:hend + 43] = 255
+    pixel_array[vstart + 327:vstart + 330, hend + 40:hend + 55] = 255
+    # draw I
+    pixel_array[vstart + 310:vstart + 330, hend + 66:hend + 70] = 255
+    pixel_array[vstart + 310:vstart + 313, hend + 60:hend + 75] = 255
+    pixel_array[vstart + 327:vstart + 330, hend + 60:hend + 75] = 255
+    # draw N
+    pixel_array[vstart + 310:vstart + 330, hend + 80:hend + 83] = 255
+    for i in range(1,12):
+        pixel_array[vstart + 311 + i:vstart + 315 + i, hend + 80 + i] = 255
+    pixel_array[vstart + 310:vstart + 330, hend + 92:hend + 95] = 255
+    # draw E
+    pixel_array[vstart + 310:vstart + 330, hend + 100:hend + 103] = 255
+    pixel_array[vstart + 310:vstart + 313, hend + 103:hend + 115] = 255
+    pixel_array[vstart + 318:vstart + 321, hend + 103:hend + 115] = 255
+    pixel_array[vstart + 327:vstart + 330, hend + 103:hend + 115] = 255
+    # draw S
+    pixel_array[vstart + 310:vstart + 321, hend + 120:hend + 123] = 255
+    pixel_array[vstart + 310:vstart + 313, hend + 123:hend + 135] = 255
+    pixel_array[vstart + 318:vstart + 321, hend + 123:hend + 135] = 255
+    pixel_array[vstart + 327:vstart + 330, hend + 120:hend + 135] = 255
+    pixel_array[vstart + 318:vstart + 330, hend + 132:hend + 135] = 255
 
+    # draw score
+    # hundreds digit
+    pixel_array[vstart + 360:vstart + 363, hend + 63:hend + 72] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 72:hend + 75] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 72:hend + 75] = 255
+    pixel_array[vstart + 378:vstart + 381, hend + 63:hend + 72] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 60:hend + 63] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 60:hend + 63] = 255
+    pixel_array[vstart + 369:vstart + 372, hend + 63:hend + 72] = 255
+
+    # tens digit
+    pixel_array[vstart + 360:vstart + 363, hend + 83:hend + 92] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 92:hend + 95] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 92:hend + 95] = 255
+    pixel_array[vstart + 378:vstart + 381, hend + 83:hend + 92] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 80:hend + 83] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 80:hend + 83] = 255
+    pixel_array[vstart + 369:vstart + 372, hend + 83:hend + 92] = 255
+
+    # ones digit
+    pixel_array[vstart + 360:vstart + 363, hend + 103:hend + 112] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 112:hend + 115] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 112:hend + 115] = 255
+    pixel_array[vstart + 378:vstart + 381, hend + 103:hend + 112] = 255
+    pixel_array[vstart + 372:vstart + 378, hend + 100:hend + 103] = 255
+    pixel_array[vstart + 363:vstart + 369, hend + 100:hend + 103] = 255
+    pixel_array[vstart + 369:vstart + 372, hend + 103:hend + 112] = 255
+    
+    pixel_array[vstart - side - space - 5:vstart - side - space, hstart - side - space - 5:hend + 160] = 255
+    pixel_array[vstart - side - space + 22 * (side + space):vstart - side - space + 22 * (side + space) + 5, hstart - side - space - 5:hend + 160] = 255
+    pixel_array[vstart - side - space - 5:vstart - side - space + 22 * (side + space), hstart - side - space - 5:hstart - side - space] = 255
+    pixel_array[vstart - side - space - 5:vstart - side - space + 22 * (side + space), hstart - side - space + 12 * (side + space):hstart - side - space + 12 * (side + space) + 5] = 255
+    pixel_array[vstart - side - space - 5:vstart - side - space + 22 * (side + space) + 5, hend + 160:hend + 165] = 255
+    pixel_array[vstart + 260:vstart + 265, hstart - side - space + 12 * (side + space):hend + 165] = 255
+
+    pixel_array[vstart - side - space:vstart, hstart - side - space:hend + side + space] = 100
+    pixel_array[vend:vend + side + space, hstart - side - space:hend + side + space] = 100
+    pixel_array[vstart:vend, hstart - side - space:hstart] = 100
+    pixel_array[vstart:vend, hend:hend + side + space] = 100
 
     pixel_array = pixel_array.T
     
